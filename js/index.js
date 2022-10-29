55ddfecd736acec60241a4b1a92b57804219f7f6
Hugo Jaca <hhjaca@gmail.com>
2022-10-29 11:48:42 -0300

    Primer desarrollo

---
+++ js/index.js
@@ -0,0 +1,60 @@
+
+
+var cad = `
+<!--CSS-->
+<style>
+    .navbar-image {
+        width: 80px;
+        height: 80px;
+        padding: 10px;
+    }
+</style>
+<h1>Proyecto de 4</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="imagenes/ep.png"
+            class="navbar-image" 
+            alt="GFG logo" />
+    </a>
+    <button class="navbar-toggler" type="button" 
+        data-toggle="collapse" 
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" 
+        aria-expanded="false" 
+        aria-label="Toggle navigation">
+          
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" 
+        id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+            <a class="nav-link active" href="index.html"> Home <span class="sr-only">(current)</span>            </a>
+            <a class="nav-link" href="about.html">About Us</a>
+           <a class="nav-link" href="location.html">Sucursales</a>
+            <a class="nav-link" href="form.html">Registrarse</a>
+            <a class="nav-link" href="Productos.html">Productos</a>
+        </div>
+    </div>
+    </nav>
+`
+document.getElementById("idheader").innerHTML = cad
+cad = `
+    <a class="redsoc" href="https://www.twitter.com/hjaca" target="_blank"><i class="fa fa-twitter"
+        aria-hidden="true"></i></a>
+    <a class="redsoc" href="https://www.facebook.com/Espirituparrillero-106080937568938" target="_blank"><i class="fa fa-facebook"
+        aria-hidden="true"></i></a>
+    <a class="redsoc" href="https://www.youtube.com/channel/UCxBnGl9qGrPGs65lsfTT3aw" target="_blank"><i class="fa fa-youtube"
+        aria-hidden="true"></i></a>
+    <a class="redsoc" href="https://www.instagram.com/espiritu.parrillero/" target="_blank"><i class="fa fa-instagram"
+        aria-hidden="true"></i></a>
+    <a class="redsoc" href="https://www.linkedin.com/in/hhjaca" target="_blank"><i
+        class="fa fa-linkedin" aria-hidden="true"></i></a>
+    <a class="redsoc" href="https://www.linkedin.com/in/hhjaca" target="_blank"><i
+        class="fa fa-linkedin" aria-hidden="true"></i></a>
+    <a class="redsoc" href="https://www.linkedin.com/in/hhjaca" target="_blank"><i
+        class="fa fa-linkedin" aria-hidden="true"></i></a>
+    <p>Derechos Reservados @2022 </p>  
+`
+document.getElementById("idfooter").innerHTML = cad
+
